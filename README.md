66d1bdf9860d11d6318494da0073f6820da0092a
Lucie Lamothe <LucieLamothe@users.noreply.github.com>
2022-02-04 16:37:15 +0100

    Added dataframe for tsv output + corrected structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # edamci
 CI python tool for the Edam Ontology
+
+EDAM_PATH=../edamontology/EDAM_dev.owl python3 -m unittest edamci_query_out.py 
--- edamci_query_out.py
+++ edamci_query_out.py
@@ -2,57 +2,56 @@ import unittest
 from rdflib import ConjunctiveGraph, Namespace
 import json
 import os
+import pandas as pd
 
-class edamQueryTest(unittest.TestCase):
-      
-    def parse_edam(self, edam_file):
-        g = ConjunctiveGraph()
-        g.parse(os.environ.get('EDAM_PATH', edam_file), format='xml')
-        return (g)
+class EdamQueryTest(unittest.TestCase):
+
+    @classmethod  
+    def setUpClass(cls):  
+        cls.edam_graph = ConjunctiveGraph()
+        cls.edam_graph.parse(os.environ.get('EDAM_PATH'), format='xml')
+        cls.report = pd.DataFrame(columns = ['Level','Test Name','Entity','Label','Debug Message'])
+        print(cls.report)
+        print("setupclass")
     
-    # def test_initialize_output(self):
-    #     with open("edamci.out", 'w') as f:
-    #         f.write("Level\tTest Name\tEntity\tLabel\tDebug Message\n")
-    #     f.close()
-    #     print("workiiing")
-
-
     def test_deprecated_replacement_obsolete(self):
         
-        g = self.parse_edam(edam_file="/home/llamothe/work/edamontology/EDAM_dev.owl")
-        
-        query = "/home/llamothe/work/ci_query/deprecated_replacement_obsolete.rq"
+        query = "queries/deprecated_replacement_obsolete.rq"
         with open(query,'r') as f:
             query_term = f.read()
-        results = g.query(query_term)
+        results = self.edam_graph.query(query_term)
         nb_err = len(results)
         f.close()
 
-        with open("edamci.out", 'a') as f:
+        for r in results:
+            new_error = pd.DataFrame([['ERROR','deprecated_replacement_obsolete',r['entity'],r['label'],(f"concept is replaced by ({r['property']}) an obsolete concept: {r['replacement']}")]], columns=['Level','Test Name','Entity','Label','Debug Message'])
+            self.__class__.report = pd.concat([self.report, new_error],  ignore_index=True) 
+            #self.report = self.report.append({'Level':'ERROR','Test Name':'deprecated_replacement_obsolete','Entity':r['entity'],'Label':r['label'],'Debug Message':(f"concept is replaced by ({r['property']}) an obsolete concept: {r['replacement']}")}, ignore_index=True)
+        
 
-            for r in results:
-                f.write(f"ERROR\tdeprecated_replacement_obsolete\t{r['entity']}\t'{r['label']}'\tconcept is replaced by ({r['property']}) an obsolete concept: {r['replacement']}'\n")
-        f.close()
+
+        print(self.__class__.report)
         
         self.assertEqual(nb_err, 0)
 
     def test_super_class_refers_to_self(self):
         
-        g = self.parse_edam(edam_file="/home/llamothe/work/edamontology/EDAM_dev.owl")
-
-        query = "/home/llamothe/work/ci_query/super_class_refers_to_self.rq"
+        query = "queries/super_class_refers_to_self.rq"
         with open(query,'r') as f:
             query_term = f.read()
 
-        results = g.query(query_term)
+        results = self.edam_graph.query(query_term)
         nb_err = len(results)
 
         for r in results:
-            print(f"{r['entity']} , super declared as superclass of itself ")
+            new_error = pd.DataFrame([['ERROR','super_class_refers_to_self',r['entity'],r['label'],'concept declared as superclass of itself']], columns=['Level','Test Name','Entity','Label','Debug Message'])
+            self.__class__.report = pd.concat([self.report, new_error],  ignore_index=True) 
+        
+        print(self.__class__.report)
+
+    
         self.assertEqual(nb_err, 0)
 
 if __name__ == '__main__':
-    with open("edamci.out", 'w') as f:
-        f.write("Level\tTest Name\tEntity\tLabel\tDebug Message\n")
-    f.close()
+
     unittest.main()
